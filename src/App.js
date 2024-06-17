c2af28141d46eddcf1e251188084b8f2a95b2191
AbhishekMangal <149200926+AbhishekMangal@users.noreply.github.com>
2024-06-17 13:16:12 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -6,27 +6,34 @@ import Navbar from './Component/Navbar';
 
 function App() {
   const [articles, setArticles] = useState([]);
-
+  const [country, setCountry] = useState('in');
+  const [category, setCategory] = useState('general')
+  const [keyWord, setkeyWord] = useState([]);
+  
+  
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const result = await axios.get("https://newsapi.org/v2/everything?q=bitcoin&apiKey=b9329e3225d746e595d53e1fa32adf03");
+       if(category!== undefined){
+        console.log(category);
+        const result = await axios.get(`https://newsapi.org/v2/top-headlines?q=${keyWord}&country=${country}&category=${category}&apiKey=b9329e3225d746e595d53e1fa32adf03`);
         setArticles(result.data.articles);
+       }
       } catch (error) {
         console.error("Error fetching data", error);
       }
     };
 
     fetchData();
-  }, []);
+  }, [category]);
 
   return (
     <>
-        <Navbar/>
+        <Navbar setCategory={setCategory} category={category}/>
     <div className="App ps-2 pe-2" style={{justifyContent:'center', alignItems:'center', display:'grid', gridTemplateColumns:'repeat(3,1fr)', gridGap:'1.5rem', padding:'25px' }}>
       {articles.map((article, index) => (
 
-        <div className="card rounded-md hover:shadow-[#0f71dd]" style={{maxHeight: '400px', overflow:'auto', height:'450px', backgroundColor:'transparent',color:'#0ea5e9'}} key={index}>
+        <div className="card rounded-md hover:shadow-[#0f71dd] bg-[#0f172abf]" style={{maxHeight: '400px', overflow:'hidden', height:'450px', backgroundColor:'transparent',color:'#0ea5e9'}} key={index}>
           <img className="card-img-top " src={article.urlToImage} alt="Card image cap" style={{width:'100%', maxHeight:'200px', height:'200px', objectFit:'cover'}} />
           <div className="card-body" style={{padding:'10px'}}>
             <h5 className="card-title">{article.title}</h5>
--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -3,31 +3,52 @@ import { IoHome } from "react-icons/io5";
 import { MdOutlineSportsCricket } from "react-icons/md";
 import { SiBookstack } from "react-icons/si";
 import { PiTelevisionSimpleFill } from "react-icons/pi";
-const Navbar = () => {
-  return (
-    <div className='NavBar  text-2xl pb-3  ps-3 pt-2 min-h-[3.5rem] mb-2 grid ' style={{gridTemplateColumns: '2fr auto'}}>
-      <div className=" text-slate-300 ">NavBar</div>
-      <div className=" flex gap-x-4 pe-10 ">
-        <div className="flex items-center">
-            <IoHome size={25} className='text-[#4E5283]' />
-            <span className='text-slate-300 hover:text-[#0ea5e9]'>Home</span>
-        </div>
-        <div className="flex items-center">
-            <MdOutlineSportsCricket size={25} className='text-[#4E5283]' />
-            <span className='text-slate-300 hover:text-[#0ea5e9]'>Sports</span>
-        </div>
-        <div className="flex items-center">
-            <SiBookstack size={25} className='text-[#4E5283]' />
-            <span className='text-slate-300 hover:text-[#0ea5e9]'>Education</span>
-        </div>
-        <div className="flex items-center">
-            <PiTelevisionSimpleFill size={25} className='text-[#4E5283]' />
-            <span className='text-slate-300 hover:text-[#0ea5e9]'>Entertainment</span>
-        </div>
+import { HiOutlineDesktopComputer } from "react-icons/hi";
+import { IoBusiness } from "react-icons/io5";
+import { CiHospital1 } from "react-icons/ci";
 
+const Navbar = ({ setCategory, category }) => {
+  const handleClick = (value) => {
+    setCategory(value);
+    console.log(value);
+    console.log(category);
+  }
+
+  return (
+    <div className='NavBar text-2xl ps-3 pt-2  grid items-center border-slate-800 border-b-2 sticky top-0 bg-[#0f172abf]' style={{ gridTemplateColumns: '0.6fr auto', height: '80px' }}>
+      <div className="text-slate-300">Khabri</div>
+      <div className="flex gap-x-4 pe-10 text-2xl">
+        <div className="flex items-center" onClick={() => handleClick('general')}>
+          <IoHome size={25} className='text-[#4E5283] mr-1' />
+          <span className='text-slate-300 hover:text-[#0ea5e9]'>Home</span>
+        </div>
+        <div className="flex items-center" onClick={() => handleClick('sports')}>
+          <MdOutlineSportsCricket size={25} className='text-[#4E5283] mr-1' />
+          <span className='text-slate-300 hover:text-[#0ea5e9]'>Sports</span>
+        </div>
+        <div className="flex items-center" onClick={() => handleClick('science')}>
+          <SiBookstack size={25} className='text-[#4E5283] mr-1' />
+          <span className='text-slate-300 hover:text-[#0ea5e9]'>Science</span>
+        </div>
+        <div className="flex items-center" onClick={() => handleClick('entertainment')}>
+          <PiTelevisionSimpleFill size={25} className='text-[#4E5283] mr-1' />
+          <span className='text-slate-300 hover:text-[#0ea5e9]'>Entertainment</span>
+        </div>
+        <div className="flex items-center" onClick={() => handleClick('technology')}>
+          <HiOutlineDesktopComputer size={25} className='text-[#4E5283] mr-1' />
+          <span className='text-slate-300 hover:text-[#0ea5e9]'>Technology</span>
+        </div>
+        <div className="flex items-center" onClick={() => handleClick('business')}>
+          <IoBusiness size={25} className='text-[#4E5283] mr-1' />
+          <span className='text-slate-300 hover:text-[#0ea5e9]'>Business</span>
+        </div>
+        <div className="flex items-center" onClick={() => handleClick('health')}>
+          <CiHospital1 size={25} className='text-[#4E5283] mr-1' />
+          <span className='text-slate-300 hover:text-[#0ea5e9]'>Health</span>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
