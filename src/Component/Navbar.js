fa50ab67b0dbef57b1bc3a352729b4f846a87e82
AbhishekMangal <149200926+AbhishekMangal@users.noreply.github.com>
2024-06-18 22:21:06 +0530

    Fifth commit

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -13,6 +13,7 @@ const Navbar = ({ setCategory, category, setKeyword }) => {
   const [inputValue, setInputValue] = useState('');
   const [menuOpen, setMenuOpen] = useState(false);
   const handleClick = (value) => {
+    console.log(value);
     setCategory(value);
     setMenuOpen(false)
   };
@@ -38,28 +39,28 @@ const Navbar = ({ setCategory, category, setKeyword }) => {
   return (
     <>
     <div className='NavBar text-lg md:text-2xl ps-3 pt-2 grid items-center border-slate-800 border-b-2 sticky top-0 bg-[#0f172abf]  ' style={{ gridTemplateColumns: 'auto 1fr auto', height: '80px' }}>
-       <div className="xl:hidden flex items-center pr-3">
+       <div className="lg:hidden flex items-center pr-3">
         <button onClick={toggleMenu} className="text-3xl text-slate-300 focus:outline-none">
           &#8801;
         </button>
       </div>
       <div className="text-slate-300 flex items-center">
         <img src={logo} alt="Logo" className='h-8 pe-3 ms-3' />
-        <span className='text-lg md:text-xl font-bold'>Khabri</span>
+        <span className='text-lg  md:text-xl font-bold '>Khabri</span>
       </div>
-      <div className="hidden xl:flex gap-x-2 px-6 text-lg cursor-pointer justify-center  xl:gap-x-4 2xl:gap-x-6">
-      <Navitem text= "Home" icon={<IoHome size={25}/>} onclick={()=>handleClick('general')} isActive={category ==='general'}/>
-      <Navitem text= "Sports" icon={<MdOutlineSportsCricket size={25}/>} onclick={()=>handleClick('general')} isActive={category ==='sports'}/>
-      <Navitem text= "Science" icon={<SiBookstack size={25}/>} onclick={()=>handleClick('general')} isActive={category ==='science'}/>
-      <Navitem text= "Entertainment" icon={<PiTelevisionSimpleFill size={25}/>} onclick={()=>handleClick('general')} isActive={category ==='entertainment'}/>
-      <Navitem text= "Technology" icon={<HiOutlineDesktopComputer size={25}/>} onclick={()=>handleClick('general')} isActive={category ==='technology'}/>
-      <Navitem text= "Business" icon={<IoBusiness size={25}/>} onclick={()=>handleClick('general')} isActive={category ==='business'}/>
-      <Navitem text= "Health" icon={<CiHospital1 size={25}/>} onclick={()=>handleClick('general')} isActive={category ==='health'}/>
+      <div className=" hidden lg:flex gap-x-2 px-6 text-lg cursor-pointer justify-center 2xl:gap-x-6">
+    <Navitem text="Home" icon={<IoHome size={25} />} onClick={() => handleClick('general')} isActive={category === 'general'} />
+     <Navitem text="Sports" icon={<MdOutlineSportsCricket size={25} />} onClick={() => handleClick('sports')} isActive={category === 'sports'} />
+     <Navitem text="Science" icon={<SiBookstack size={25} />} onClick={() => handleClick('science')} isActive={category === 'science'} />
+     <Navitem text="Entertainment" icon={<PiTelevisionSimpleFill size={25} />} onClick={() => handleClick('entertainment')} isActive={category === 'entertainment'} />
+     <Navitem text="Technology" icon={<HiOutlineDesktopComputer size={25} />} onClick={() => handleClick('technology')} isActive={category === 'technology'} />
+     <Navitem text="Business" icon={<IoBusiness size={25} />} onClick={() => handleClick('business')} isActive={category === 'business'} />
+     <Navitem text="Health" icon={<CiHospital1 size={25} />} onClick={() => handleClick('health')} isActive={category === 'health'} />
       </div>
 
-      <div className="flex items-center bg-[#0f172abf] px-3 justify-start ">
+      <div className=" flex items-center bg-[#0f172abf] px-3 justify-start ">
         <AiOutlineSearch size={25} className='text-slate-300 mr-2' />
-        <input type="search" placeholder="Search" className="w-80 bg-[#0f172abf] focus:outline-none border-none justify-center rounded text-slate-300" value={inputValue} onChange={handleChange}
+        <input type="search" placeholder="Search" className="w- 60 bg-[#0f172abf] focus:outline-none border-none justify-center rounded text-slate-300" value={inputValue} onChange={handleChange}
         />
         <button className='h-10 w-12 flex items-center justify-center rounded-e-full bg-zinc-800' onClick={handleSearch}>
           <AiOutlineSearch className='text-xl text-slate-300' />
@@ -67,7 +68,7 @@ const Navbar = ({ setCategory, category, setKeyword }) => {
       </div>
       {/* Mobile toggle button*/}
    {menuOpen && (
-     <div className="xl:hidden absolute top-20 left-0 right-0 bg-[#0f172abf] p-4 z-20">
+     <div className="lg:hidden absolute top-20 left-0 right-0 bg-[#0f172abf] p-4 z-20">
      <Navitem text="Home" icon={<IoHome size={25} />} onClick={() => handleClick('general')} isActive={category === 'general'} />
      <Navitem text="Sports" icon={<MdOutlineSportsCricket size={25} />} onClick={() => handleClick('sports')} isActive={category === 'sports'} />
      <Navitem text="Science" icon={<SiBookstack size={25} />} onClick={() => handleClick('science')} isActive={category === 'science'} />
--- src/Home.js
+++ src/Home.js
@@ -1,51 +1,87 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Navbar from './Component/Navbar';
-import img from './Images/null images.jpg'
+import img from './Images/null images.jpg';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 const Home = () => {
-    const [articles, setArticles] = useState([]);
+  const [articles, setArticles] = useState([]);
   const [country, setCountry] = useState('in');
-  const [category, setCategory] = useState('general')
+  const [category, setCategory] = useState('general');
   const [keyWord, setKeyword] = useState('');
+  const [page, setPage] = useState(2);
+  const [totalResult, setTotalResult] = useState(0);
   const navigate = useNavigate();
   
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-       if(category){
+  const fetchData = async () => {
+    try {
+      const result = await axios.get(`https://newsapi.org/v2/top-headlines?q=${keyWord}&country=${country}&category=${category}&apiKey=c5de9d90d852436d868f5f1e17e4b1f3&page=${page}`);
+      setArticles(prevArticles => [...prevArticles, ...result.data.articles]);
+      setPage(prevPage => prevPage + 1);
+    } catch (error) {
+      console.error("Error fetching data", error);
+    }
+  };
 
-        const result = await axios.get(`https://newsapi.org/v2/top-headlines?q=${keyWord}&country=${country}&category=${category}&apiKey=c5de9d90d852436d868f5f1e17e4b1f3`);
+  useEffect(() => {
+    const initialFetch = async () => {
+      try {
+        const result = await axios.get(`https://newsapi.org/v2/top-headlines?q=${keyWord}&country=${country}&category=${category}&apiKey=c5de9d90d852436d868f5f1e17e4b1f3&page=1`);
         setArticles(result.data.articles);
-       setKeyword('');
-       }
+        setPage(2);
+        setTotalResult(result.data.totalResults)
+     
       } catch (error) {
         console.error("Error fetching data", error);
       }
     };
 
-    fetchData();
-  }, [category, keyWord, keyWord.length != 0]);
+    initialFetch();
+  }, [category, keyWord, country]);
+
   return (
     <>
-     <Navbar setCategory={setCategory} category={category} setKeyword={setKeyword} keyWord={keyWord}/>
-    <div className="App p-4 grid md:grid-cols-2 gap-2 lg:grid-cols-4 gap-4 w-100">
-      {articles.map((article, index) => (
-
-        <div className="card rounded-lg hover:shadow-[#0f71dd]  p-4 bg-[#0f172abf] bg-opacity-70" style={{maxHeight: '400px', overflow:'hidden',color:'#0ea5e9'}} key={index}>
-          <img className="card-img-top border-gray-700 border-b-2 " src={article.urlToImage? article.urlToImage :img} alt="Card image cap" style={{width:'100%', maxHeight:'200px', height:'200px', objectFit:'cover'}} />
-          <div className="card-body p-2" style={{padding:'10px'}}>
-            <a className="card-title text-blue-300 hover:text-blue-500 block" href={`${article.url}`}>{article.title}</a>
-            <br />
-            <p className="card-text text-gray-300 hover:text-gray-200 block mt-2 truncate"  >{article.description}</p>
-       
-          </div>
+      <Navbar setCategory={setCategory} category={category} setKeyword={setKeyword} keyWord={keyWord} />
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchData}
+        hasMore={articles.length !== totalResult}
+        loader={<h4 className='text-center text-3xl text-slate-300 p-4'>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: 'center' }} className='text-slate-300 text-2xl p-5'>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+        refreshFunction={() => {
+          setArticles([]);
+          setPage(page+1);
+          fetchData();
+        }}
+        pullDownToRefresh
+        pullDownToRefreshThreshold={10}
+        pullDownToRefreshContent={
+          <h3 style={{ textAlign: 'center' }}>&#8595; Pull down to refresh</h3>
+        }
+        releaseToRefreshContent={
+          <h3 style={{ textAlign: 'center' }}>&#8593; Release to refresh</h3>
+        }
+      >
+        <div className="App p-4 grid md:grid-cols-2 gap-2 lg:grid-cols-4 gap-4 w-100">
+          {articles.map((article, index) => (
+            <div className="card rounded-lg hover:shadow-[#0f71dd]  p-4 bg-[#0f172abf] bg-opacity-70" style={{ maxHeight: '400px', overflow: 'hidden', color: '#0ea5e9' }} key={index}>
+              <img className="card-img-top border-gray-700 border-b-2" src={article.urlToImage ? article.urlToImage : img} alt="Card image cap" style={{ width: '100%', maxHeight: '200px', height: '200px', objectFit: 'cover' }} />
+              <div className="card-body p-2" style={{ padding: '10px' }}>
+                <a className="card-title text-blue-300 hover:text-blue-500 block" href={`${article.url}`}>{article.title}</a>
+                <br />
+                <p className="card-text text-gray-300 hover:text-gray-200 block mt-2 truncate">{article.description}</p>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      </InfiniteScroll>
     </>
-    
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
